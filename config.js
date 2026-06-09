da248e3205412b51e1fda8142973e0f65c943923
segev <smhl3052000@gmail.com>
2026-06-09 16:32:53 +0300

    single API_base

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-const API_BASE = window.location.hostname === 'localhost'
-  ? 'http://localhost:3000'
-  : 'https://learning-app-v1-test-backend.onrender.com';
+const API_BASE = 'https://learning-app-v1-test-backend.onrender.com';
