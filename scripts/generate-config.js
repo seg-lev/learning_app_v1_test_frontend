e0b1471c244f4db0eb1522b6613ddb6f239fe90c
segev <smhl3052000@gmail.com>
2026-06-09 12:03:39 +0300

    netlify undone

--- scripts/generate-config.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const apiUrl = process.env.API_URL || 'http://localhost:3000';
-const out = path.join(__dirname, '..', 'config.js');
-
-fs.writeFileSync(out, `window.API_BASE = ${JSON.stringify(apiUrl)};\n`);
-console.log('Wrote config.js with API_BASE =', apiUrl);
